2c406d0f396826c91225d1ada610173fde339b7a
Vadym <wadedevelop@gmail.com>
2019-05-18 17:22:34 +0300

    Using renamed ConsolePrinter

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from console_printer import Printer
+from printers import ConsolePrinter
 from game import Game
 
 '''
-Simple Tic-Tac-Toe console game implementation
+    Simple Tic-Tac-Toe console game implementation
 '''
 if __name__ == "__main__":
     # Create a new game instance and specify the Printer to be used
-    game = Game(Printer)
+    game = Game(ConsolePrinter)
 
     # Start the game
     game.run()
